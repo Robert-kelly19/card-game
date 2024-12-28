688f42bb241bfe30fa27cb2beabca6e044509fe2
kellyhandy31@gmail.com <kellyhandy31@gmail.comgit config --global user.name Robert-kelly19>
2024-12-28 13:05:28 +0100

    game functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card')
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.toggle('flip')
   })
-});
+})
