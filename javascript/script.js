61ce565cb12349f852dc0dc563e1eea45de73dea
kellyhandy31@gmail.com <kellyhandy31@gmail.comgit config --global user.name Robert-kelly19>
2024-12-30 23:01:44 +0100

    js

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,20 @@
 const cards = document.querySelectorAll('.card')
-const turn = document.querySelectorAll('p')
+const image = document.querySelectorAll('img')
+console.log(image)
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     card.classList.toggle('flip')
+    for (let i = 0; i < 1; i++) {
+      if (image.length) {
+        if (image[i].id===image[i].id) {
+          console.log("same")
+        }else{
+          console.log('wrong')
+        }
+      }
+    }
   })
 })
+
+
 
